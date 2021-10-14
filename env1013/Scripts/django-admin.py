d85f09a3aedcc063de3d8838edb612ae6f6e6cd8
GL <2756854126@qq.com>
2021-10-14 09:39:44 +0800

    添加项目文件。

---
+++ env1013/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Microsoft\source\repos\DjangoWebProject1\env1013\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
